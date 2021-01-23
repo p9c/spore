296a69e2594af119a0db8f4da25624ecbce15bd0
Loki Verloren <stalker.loki@protonmail.ch>
2021-01-23 22:04:59 +0100

    a few more notes

--- cmd/spore/cli.go
+++ cmd/spore/cli.go
@@ -17,6 +17,9 @@ func (c *CLI) Run() int {
 	Debug("running", os.Args[1:])
 	// first we need to check we have go in the shell environment, fetch and unpack it otherwise, write config setting
 	// which url and hash of package, write command to set new URL and hash for Go distribution.
+	// maybe also command to list and select other than the main/master branch
+	// packages will need commit/tag, and go version specifications, and multiple go version builds, to pull and cache
+	// go distribution when required and not cached
 	// check if we already have package and the version is current
 	// storage for packages - path and then git commit hash
 	// prune binary cache. keep whole git repo tied to package, and refer to cache if base is requested again
