acd352419b5f9a99d6c60452016725a2e4f09615
Loki Verloren <stalker.loki@protonmail.ch>
2021-01-23 12:11:59 +0100

    updating stroy with more logical command names, separating commands spec

--- stroy/commands.go
+++ stroy/commands.go
@@ -4,10 +4,15 @@ var Commands = map[string][]string{
 	"install": {
 		`go install -v %ldflags .`,
 	},
+	"run": {
+		`go install -v %ldflags .`,
+		`reset`,
+		`spore`,
+	},
 	"windows": {
 		`go build -v -ldflags="-H windowsgui" %ldflags"`,
 	},
-	"stroy": {
+	"update": {
 		"go install -v %ldflags ./stroy/.",
 	},
 }
