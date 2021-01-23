dbdd285b5bfa273ecf466846aa4c747337a0866a
Loki Verloren <stalker.loki@protonmail.ch>
2021-01-23 22:01:25 +0100

    notes about what cli needs to do

--- cmd/spore/cli.go
+++ cmd/spore/cli.go
@@ -15,7 +15,14 @@ func NewCLI(s *Shell) *CLI {
 
 func (c *CLI) Run() int {
 	Debug("running", os.Args[1:])
-	// start by fetching the package directory
-	
+	// first we need to check we have go in the shell environment, fetch and unpack it otherwise, write config setting
+	// which url and hash of package, write command to set new URL and hash for Go distribution.
+	// check if we already have package and the version is current
+	// storage for packages - path and then git commit hash
+	// prune binary cache. keep whole git repo tied to package, and refer to cache if base is requested again
+	// build cache management command - set version to build and use by git commit or tag
+	// `go build github.com/org/repo`
+	// move binary to binary cache
+	// run binary with given parameters
 	return 0
 }
